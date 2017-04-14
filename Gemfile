1421faa14bbb4e14916907612fd094f53500dcc8
giridharr <giridar@codingmart.com>
2017-04-14 15:58:06 +0530

    change sql to mysql database

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem 'devise'
+gem 'cancancan', '~> 1.10'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
@@ -31,7 +32,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'mysql2'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20170414094101) do
 
-  create_table "employees", force: :cascade do |t|
+  create_table "employees", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email"
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                        default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -27,14 +27,14 @@ ActiveRecord::Schema.define(version: 20170414094101) do
     t.string   "last_name"
     t.string   "gender"
     t.string   "designation"
-    t.text     "address"
+    t.text     "address",                limit: 65535
     t.string   "mobile"
     t.date     "date_of_join"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
     t.string   "employee_id"
-    t.index ["email"], name: "index_employees_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_employees_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true, using: :btree
   end
 
 end
